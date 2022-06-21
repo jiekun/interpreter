2dc6f7cd7da28984019146e6c25fa46ae206b9f0
jiekun.zhu <jiekun.zhu@shopee.com>
2022-06-21 20:10:38 +0800

    added parse int

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jiekun/interpreter/ast"
 	"github.com/jiekun/interpreter/lexer"
 	"github.com/jiekun/interpreter/token"
+	"strconv"
 )
 
 const (
@@ -45,6 +46,7 @@ func New(l *lexer.Lexer) *Parser {
 	}
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	p.nextToken()
 	p.nextToken()
@@ -145,6 +147,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
