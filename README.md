28c65f0848becba060ae964fc25289f70a02b157
Jiekun <jiekun.dev@gmail.com>
2023-07-13 10:20:12 +0800

    2.8.2 added support for paren

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ Hello {username}! This is the Monkey programming language!
 Feel free to type in commands
 >>
 ```
+
+## Developing and Testing
+It's pretty interesting to add new features. This project is **Test-Deiven**. 
+You should write test cases first, and then run those tests with:
+```shell
+go test -coverpkg=./... -cover -covermode=atomic -gcflags=all=-l ./... -coverprofile=coverage.txt
+```
+
+Now you should be able to see some error logs. Fix them with new features, and run the test again.
--- parser/parser.go
+++ parser/parser.go
@@ -63,6 +63,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupExpression)
 	p.registerInfix(token.EQ, p.parseInfixExpression)
 	p.registerInfix(token.NEQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
@@ -228,6 +229,18 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
